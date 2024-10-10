94771af9e5dac76a6ec18e6a897b9ac9fe8d8785
Hieu <hieun0981@gmail.com>
2024-10-10 21:13:07 +0700

    edit

---
+++ trangchu.js
@@ -0,0 +1,7 @@
+
+var account = document.getElementById("tk");
+var title_account = document.getElementById("list_acc");
+
+account.addEventListener('click',()=> {
+    title_account.classList.add("none");
+});
